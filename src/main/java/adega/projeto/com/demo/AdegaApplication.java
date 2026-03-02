f5bc35d0be2371720c4a815d82f71aca230bfbff
Leonardo Aquino <aquinoleonardo000@gmail.com>
2026-03-02 12:52:59 -0300

    Initial commit: Corrigindo estrutura de pastas

---
+++ src/main/java/adega/projeto/com/demo/AdegaApplication.java
@@ -0,0 +1,13 @@
+package adega.projeto.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdegaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/adega/projeto/com/demo/AdegaApplicationTests.java
@@ -0,0 +1,13 @@
+package adega.projeto.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
